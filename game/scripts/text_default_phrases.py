ed0a060dc39a0cb47b9c2053251c850971140f9c
OldHuntsman <old_huntsman@yahoo.com>
2016-07-23 00:54:15 +0300

    ридми

--- game/scripts/text_default_phrases.py
+++ game/scripts/text_default_phrases.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
 import collections
 phrases = {'english':{'antosha': "I'm back again, i'm slave again..."},
-            'rus': {'antosha': u"Я вернулся, я снова послушный раб..."}
+           'rus': {'antosha': u"Я вернулся, я снова послушный раб..."}
             }
